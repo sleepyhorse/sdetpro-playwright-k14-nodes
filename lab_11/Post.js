d8d4ea5edd65a117857e66e1b1519ef2e035dd54
sleepyhorse <uannd.hus@gmail.com>
2024-03-31 12:28:27 +0000

    Update Post.js

--- lab_11/Post.js
+++ lab_11/Post.js
@@ -1,14 +1,39 @@
 class Post {
-    constructor(userId, postId, title, body) {
-        this.userId = userId;
-        this.postId = postId;
-        this.title = title;
-        this.body = body;
+    constructor(userId, id, title, body){
+        this._userId = userId;
+        this._id = id;
+        this._title = title;
+        this._body = body;
+    }
+    get userId(){
+        return this._userId;
     }
 
-    toString() {
-        return `Post ${this.postId} by User ${this.userId}: ${this.title}\n${this.body}`;
+    set userId(userId){
+        return this._userId = userId;
+    }
+    get id(){
+        return this._id;
+    }
+
+    set id(id){
+        return this._id = id;
+    }
+
+    get title(){
+        return this._title;
+    }
+
+    set title(title){
+        return this._title = title;
+    }
+
+    get body(){
+        return this._body;
+    }
+
+    set body(body){
+        return this._body = body;
     }
 }
-
-module.exports = Post;
+module.exports = Post
