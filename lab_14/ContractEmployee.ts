f45e7b7dbeb4fc68c5f5a9e6bfc02a1d92123fe3
sleepyhorse <uannd.hus@gmail.com>
2024-04-02 02:44:21 +0000

    Delete lab_14/ContractEmployee.ts

--- lab_14/ContractEmployee.ts
+++
@@ -1,9 +0,0 @@
-import Employee from "./Employee";
-
-
-export default class ContractEmployee extends Employee {
-    static contractSal = 4000;
-    constructor(name: string) {
-        super(name, ContractEmployee.contractSal)
-    }
-}
